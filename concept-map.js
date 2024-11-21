6404dddd7f94ed103f42c34a0b6f7b089e7a1458
Gaurav Sharan Srivastava <158238900+CalBlitz@users.noreply.github.com>
2024-11-20 23:05:12 -0800

    Update concept-map.js

--- concept-map.js
+++ concept-map.js
@@ -2,7 +2,7 @@ function showDefinition(definition) {
     // Find the definition box closest to the clicked word
     const definitionBox = event.target.parentElement.nextElementSibling;
     definitionBox.textContent = definition;
-  }
+  };
 
 const words = document.querySelectorAll('.word-cloud .word');
 
